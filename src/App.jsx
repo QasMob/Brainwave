ac17aec67051ca1e53c6c64999501a7f33a7bb49
Mobeen Qasim <mobeenqq@gmail.com>
2024-07-31 19:30:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits.jsx";
+import Button from "./components/Button";
+import Collaboration from "./components/Collaboration.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Services from "./components/Services.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
